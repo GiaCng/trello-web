90289a629fd03fe011ca514f2f275e8ba06b50f2
GiaCng <147374173+GiaCng@users.noreply.github.com>
2024-09-13 09:34:27 +0700

    Refactor Project Structure

---
+++ src/pages/Auth/index.jsx
@@ -0,0 +1,1 @@
+//Authencation - Sign In Sign Up
---
+++ src/pages/Boards/_id.jsx
@@ -0,0 +1,17 @@
+// Board Details
+import Container from '@mui/material/Container'
+import BoardBar from './Boardbar'
+import AppBar from '../../components/AppBar'
+import BoardContent from './BoardContent'
+
+function Board() {
+  return (
+    <Container disableGutters maxWidth={false} sx={{ height: '100vh' }}>
+      <AppBar />
+      <BoardBar />
+      <BoardContent />
+    </Container>
+  )
+}
+
+export default Board
