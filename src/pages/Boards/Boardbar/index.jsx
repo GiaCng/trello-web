679c7601ede0661439ab173b2e465ef9d10c64a6
GiaCng <147374173+GiaCng@users.noreply.github.com>
2024-09-27 22:37:54 +0700

    Code xog board content

--- src/pages/Boards/Boardbar/index.jsx
+++ src/pages/Boards/Boardbar/index.jsx
@@ -38,7 +38,9 @@ function BoardBar() {
       paddingX: 2,
       overflowX: 'auto',
       bgcolor: (theme) => (theme.palette.mode == 'dark' ? '#34495e' : '#1976b2'),
-      borderBottom: '1px solid white'
+      borderBottom: '1px solid white',
+      '&::-webkit-scrollbar-track': { m: 2 }
+
     }}>
       <Box sx={{ display: 'flex', alignItems: 'center', gap: 2 }} >
         <Chip
@@ -92,7 +94,10 @@ function BoardBar() {
               width: 34,
               height: 34,
               fontSize: 16,
-              border: 'none'
+              border: 'none',
+              color: 'white',
+              cursor: 'pointer',
+              '&:first-of-type': { bgcolor: '#a4b0be'}
             }
           }}
         >
