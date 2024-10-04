ef261383189a548a206841a9af827b26cc9efd18
GiaCng <147374173+GiaCng@users.noreply.github.com>
2024-10-04 09:43:57 +0700

    Done FE

--- src/pages/Boards/BoardContent/ListColumns/Columns/ListCards/Cards/Cards.jsx
+++ src/pages/Boards/BoardContent/ListColumns/Columns/ListCards/Cards/Cards.jsx
@@ -38,7 +38,10 @@ function Card({ card }) {
       sx={{
         cursor: 'pointer',
         boxShadow: '0 1px 1px rgba(0, 0, 0, 0.2)',
-        overflow: 'unset'
+        overflow: 'unset',
+        // display: card?.FE_PlaceholderCard ? 'none' : 'block'
+        opacity: card.FE_PlaceholderCard ? '0' : '1',
+        height: card?.FE_Placeholder ? '4px' : 'unset'
       }}>
       {card?.cover && <CardMedia sx={{ height: 140 }} image={card?.cover} />}
       <CardContent sx={{
